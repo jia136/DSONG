8afaa3f5d88773848557c13159f8aabb0079f634
jia136 <50610014+jia136@users.noreply.github.com>
2022-07-10 23:44:32 +0200

    Delete filter_iir_project directory

--- filter_iir_project/self_purging_redundancy_iir/transposed_direct_form_2/sources/arrays_package.vhd
+++
@@ -1,7 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-package arrays_package is
-    type array_type is array (natural range <>) of std_logic_vector(31 downto 0);
-end arrays_package;
